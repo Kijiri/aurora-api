71ca595bdd2fe1706bdae71384dfa25d3747ce97
Richie Rizal <richierizal127@gmail.com>
2025-02-12 21:28:03 +0100

    add logintype to regular register

--- src/main/java/com/kijiri/aurora/api/service/AuthenticationService.java
+++ src/main/java/com/kijiri/aurora/api/service/AuthenticationService.java
@@ -44,15 +44,16 @@ public class AuthenticationService {
         Role role = roleService.findRoleByName(RoleType.USER);
 
         User user = User.builder()
-            .firstName(request.getFirstName())
-            .lastName(request.getLastName())
-            .userName(request.getUserName())
-            .email(request.getEmail())
-            .hashedPassword(passwordEncoder.encode(request.getPassword()))
-            //TODO email validation from unverified to verified?
-            .status(UserStatus.VERIFIED)
-            .roles(List.of(role))
-            .build();
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .userName(request.getUserName())
+                .email(request.getEmail())
+                .hashedPassword(passwordEncoder.encode(request.getPassword()))
+                .loginType(LoginType.REGULAR)
+                //TODO email validation from unverified to verified?
+                .status(UserStatus.VERIFIED)
+                .roles(List.of(role))
+                .build();
 
         userRepository.save(user);
         log.info("User registered successfully with email: {}", request.getEmail());
@@ -62,10 +63,10 @@ public class AuthenticationService {
         log.info("Authenticating user with login: {}", request.getLogin());
 
         Authentication auth = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                request.getLogin(),
-                request.getPassword()
-            )
+                new UsernamePasswordAuthenticationToken(
+                        request.getLogin(),
+                        request.getPassword()
+                )
         );
 
         User user = (User) auth.getPrincipal();
@@ -76,12 +77,12 @@ public class AuthenticationService {
 
     public AuthenticationResponse authenticateByGoogle(Oauth2Request oauth2Request) {
         GoogleUser googleUser = googleAPIService.fetchGoogleUser(oauth2Request.getAuthCode(),
-            oauth2Request.getCodeVerifier());
+                oauth2Request.getCodeVerifier());
         if (!googleUser.getEmail_verified()) {
             throw new ForbiddenException(EMAIL_UNVERIFIED, "User has unverified Gmail");
         }
         User user = userRepository.findByEmail(googleUser.getEmail())
-            .orElseGet(() -> registerGoogleUser(googleUser));
+                .orElseGet(() -> registerGoogleUser(googleUser));
 
         return getAuthenticationResponse(user);
     }
@@ -99,16 +100,16 @@ public class AuthenticationService {
         if (userRepository.existsByEmail(request.getEmail())) {
             log.warn("Email already taken: {}", request.getEmail());
             throw new DuplicateException(
-                EMAIL_TAKEN,
-                String.format("Email is taken for: %s", request.getEmail())
+                    EMAIL_TAKEN,
+                    String.format("Email is taken for: %s", request.getEmail())
             );
         }
 
         if (userRepository.existsByUserName(request.getUserName())) {
             log.warn("Username already taken: {}", request.getUserName());
             throw new DuplicateException(
-                USERNAME_TAKEN,
-                String.format("Username is taken for: %s", request.getUserName())
+                    USERNAME_TAKEN,
+                    String.format("Username is taken for: %s", request.getUserName())
             );
         }
     }
@@ -116,13 +117,13 @@ public class AuthenticationService {
     private User registerGoogleUser(GoogleUser googleUser) {
         Role role = roleService.findRoleByName(RoleType.USER);
         User user = User.builder()
-            .firstName(googleUser.getGiven_name())
-            .lastName(googleUser.getFamily_name())
-            .email(googleUser.getEmail())
-            .status(UserStatus.VERIFIED)
-            .roles(List.of(role))
-            .loginType(LoginType.GOOGLE)
-            .build();
+                .firstName(googleUser.getGiven_name())
+                .lastName(googleUser.getFamily_name())
+                .email(googleUser.getEmail())
+                .status(UserStatus.VERIFIED)
+                .roles(List.of(role))
+                .loginType(LoginType.GOOGLE)
+                .build();
         return userRepository.save(user);
     }
 
@@ -131,13 +132,13 @@ public class AuthenticationService {
         String refreshToken = jwtService.generateRefreshToken(user);
 
         UserDTO userDTO = UserDTO.builder()
-            .id(user.getId()).build();
+                .id(user.getId()).build();
 
         return AuthenticationResponse.builder()
-            .message("Login successful")
-            .userDTO(userDTO)
-            .accessToken(accessToken)
-            .refreshToken(refreshToken)
-            .build();
+                .message("Login successful")
+                .userDTO(userDTO)
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
     }
 }
